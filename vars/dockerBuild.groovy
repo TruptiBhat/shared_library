9eba559a702333c9820e7fc265044761dc7fed69
TruptiBhat <42669360+TruptiBhat@users.noreply.github.com>
2023-12-15 16:12:21 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
-def call(){
-    def customImage = docker.build('ahmedwahi314/petclinic', "./docker")
+def call(Map config = [:]){
+    def customImage = docker.build(${config.buildImage}, "${config.filePath}")
     docker.withRegistry('https://ahmed314.azurecr.io', 'acr') {
     customImage.push("${env.BUILD_NUMBER}")
 }
